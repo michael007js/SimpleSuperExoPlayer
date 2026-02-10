267c7e333d520003d271c1ada01579ac88af0940
michael007js <shill@eagersoft.cn>
2026-02-10 16:24:33 +0800

    初始化

--- app/src/main/java/com/sss/michael/demo/MyApplication.java
+++ app/src/main/java/com/sss/michael/demo/MyApplication.java
@@ -2,11 +2,13 @@ package com.sss.michael.demo;
 
 import android.app.Application;
 
+import com.sss.michael.exo.ExoConfig;
+
 public class MyApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-
+        ExoConfig.init(this, "exo_config", true);
     }
 }
